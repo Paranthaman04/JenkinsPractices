c097e5ff932198806582c9d958537bbbe0c67ae6
Paranthaman04 <90240367+Paranthaman04@users.noreply.github.com>
2021-09-26 22:34:41 +0530

    Create SampleProgms.java

---
+++ SampleProgms.java
@@ -0,0 +1,7 @@
+public class HelloGuys {
+  
+      public static void main(String[] arg){
+        System.out.println("Hello ramu");
+        System.out.println("Hello janu");
+        System.out.println("Hello ramesh");
+      }
